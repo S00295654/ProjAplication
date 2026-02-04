ecc0d4f6d5289ac4dd3ef2c5450d1559e8633ecf
Florent Fossati <S00295654@atu.ie>
2026-02-04 11:57:35 +0000

    Trying to show user name

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,9 +20,11 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = Application.Current;
         }
     }
 }
---
+++ WpfApp1/game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input.StylusPlugIns;
+
+namespace WpfApp1
+{
+    public class Game
+    {
+        public string Name;
+        public int release;
+        public List<string> tags;
+        public List<string> genre;
+        public List<string> device;
+        public int Time;
+
+        public Game(string name, int release, List<string> tags, List<string> device, List<string> genre, int time)
+        {
+            Name = name;
+            this.release = release;
+            this.tags = tags;
+            this.device = device;
+            this.genre = genre;
+            if (time < 0)
+                this.Time = -time;
+            else 
+                this.Time = time;
+        }
+    }
+}
